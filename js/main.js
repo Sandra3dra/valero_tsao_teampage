6f679f2f100bdbc2f72c25f6e334bfed76be7308
SandraTsao <s_tsao@fanshaweonline.ca>
2019-03-14 00:13:24 -0400

    js added everything works fine

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(() => {
+	let toSnadra = document.querySelector(".changebiotoS"),
+		toLuisa = document.querySelector(".changebiotoL");
+
+	function changetoS() {
+		document.getElementById("memberImg").src="images/sandra_tsao.png";
+		document.querySelector(".lPart").classList.add("hidden");
+		document.querySelector(".sPart").classList.remove("hidden");
+		document.querySelector(".name").textContent="SANDRA TSAO";
+		document.querySelector(".buttontoS").classList.add("hidden");
+		document.querySelector(".buttontoL").classList.remove("hidden");
+	}
+
+	toSnadra.addEventListener("click", changetoS);
+
+	function changetoL() {
+		document.getElementById("memberImg").src="images/luisa_valero.png";
+		document.querySelector(".sPart").classList.add("hidden");
+		document.querySelector(".lPart").classList.remove("hidden");
+		document.querySelector(".name").textContent="LUISA VALERO";
+		document.querySelector(".buttontoS").classList.remove("hidden");
+		document.querySelector(".buttontoL").classList.add("hidden");
+	}
+
+	toLuisa.addEventListener("click", changetoL);
+
+
+})();
