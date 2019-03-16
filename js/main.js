e955003c3a47370c2e2fb5015e0c1e6f6e7ced5a
SandraTsao <s_tsao@fanshaweonline.ca>
2019-03-15 23:09:09 -0400

    js for the name colour changes

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 		document.querySelector(".lPart").classList.add("hidden");
 		document.querySelector(".sPart").classList.remove("hidden");
 		document.querySelector(".name").textContent="SANDRA TSAO";
+		document.querySelector(".name").classList.add("forS");
+		document.querySelector(".question").classList.add("qforS");
 		document.querySelector(".buttontoS").classList.add("hidden");
 		document.querySelector(".buttontoL").classList.remove("hidden");
 	}
@@ -18,6 +20,8 @@
 		document.querySelector(".sPart").classList.add("hidden");
 		document.querySelector(".lPart").classList.remove("hidden");
 		document.querySelector(".name").textContent="LUISA VALERO";
+		document.querySelector(".name").classList.remove("forS");
+		document.querySelector(".question").classList.remove("qforS");
 		document.querySelector(".buttontoS").classList.remove("hidden");
 		document.querySelector(".buttontoL").classList.add("hidden");
 	}
